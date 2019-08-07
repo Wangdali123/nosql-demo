9168ef4fc05401d09891d518afc51467c0026f4e
lide <lide@sodo-tech.com>
2019-08-07 23:53:31 +0800

    测试jenkins 屏蔽x

--- src/test/java/com/wdl/nosql/NosqlApplicationTests.java
+++ src/test/java/com/wdl/nosql/NosqlApplicationTests.java
@@ -1,39 +1,39 @@
-//package com.wdl.nosql;
-//
-//import com.wdl.nosql.domain.dao.UserMongoRepository;
-//import com.wdl.nosql.domain.pojo.User;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.concurrent.ConcurrentHashMap;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class NosqlApplicationTests {
-//
+package com.wdl.nosql;
+
+import com.wdl.nosql.domain.dao.UserMongoRepository;
+import com.wdl.nosql.domain.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NosqlApplicationTests {
+
 //    @Autowired
 //    private UserMongoRepository userMongoRepository;
 //
 //    private ConcurrentHashMap concurrentHashMap;
-//
-//    @Test
-//    public void contextLoads() {
+
+    @Test
+    public void contextLoads() {
+    }
+
+//    public void testRedisTemplate(){
+//        redisTemplate.opsForValue().in; //opsForxx xx对应着redis中的数据结构
+//        redisTemplate.delete("");
 //    }
-//
-////    public void testRedisTemplate(){
-////        redisTemplate.opsForValue().in; //opsForxx xx对应着redis中的数据结构
-////        redisTemplate.delete("");
-////    }
-//
+
 //    @Test
 //    public void testMongo(){
 //        List<User> userList = new ArrayList<>(50);
@@ -50,5 +50,5 @@
 ////        System.out.println(userList);
 //        //
 //    }
-//
-//}
+
+}
